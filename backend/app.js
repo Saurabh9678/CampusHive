5bb79fb0208b5ad9592fccddfa39770467621f48
Saurabh <gohainsaurabh@gmail.com>
2023-04-28 15:47:13 +0530

    added socket for group chat

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,8 @@ const errorMiddleware = require("./middleware/error");
 const app = express();
 // CORS error fix..
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'http://localhost:3000',
+    methods: ["GET","POST", "PUT", "DELETE"], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
@@ -16,8 +17,6 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 //Route Imports
 const userRoutes = require("./routes/userRoutes");
-
-
 
 app.use("/api/v1", userRoutes);
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-
+const server = require("http").createServer(app)
+const {Server} = require("socket.io")
+const Event =  require("../event")
 const connectDataBase = require("./config/database");
 
 //Handling Uncaught exception
@@ -16,8 +18,29 @@ dotenv.config({ path: "./config/config.env" });
 //Connecting to database
 connectDataBase();
 
+const io = new Server(server)
 
-const server = app.listen(process.env.PORT, () => {
+io.on(Event.CONNECTION, (socket)=>{
+  console.log(`User connected id: ${socket.id}`);
+
+  socket.on(Event.JOIN_ROOM, (data)=>{
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  })
+  
+  socket.on(Event.SEND_MESSAGE, (data)=>{
+      socket.to(data.room).emit("receive_message", data)
+  })
+
+  socket.on(Event.DISCONNECT,()=>{
+      console.log(`User disconnected id: ${socket.id}`);
+  })
+})
+
+
+
+
+server.listen(process.env.PORT, () => {
   console.log(`Server is running in http://localhost:${process.env.PORT}`);
 });
 
